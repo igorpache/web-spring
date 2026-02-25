67d8cb9109bfff9d558951af2e5a510cd0bb7161
Igor Pache <devigorpache@gmail.com>
2026-02-25 16:23:39 -0300

    OrderStatus enum

--- src/main/java/com/coursejava/course/entities/Order.java
+++ src/main/java/com/coursejava/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.coursejava.course.entities;
 
+import com.coursejava.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -18,6 +19,9 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -25,12 +29,12 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -55,6 +59,15 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null)
+            this.orderStatus = orderStatus;
     }
 
     @Override
---
+++ src/main/java/com/coursejava/course/entities/enums/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.coursejava.course.entities.enums;
+
+public enum OrderStatus {
+
+    HOMOLOGATION,
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+
+}
